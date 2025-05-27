58e47455a50b1f66b5eb903ffd0ccaaf2cd37ede
Vaishnavi Nimse <vaishnavinimse8797@gmail.com>
2025-05-27 18:57:03 +0530

    Update assign3task1.py

--- assign3task1.py
+++ assign3task1.py
@@ -1,4 +1,3 @@
-
 def factorial(n):
     if n==0:
         return 1
@@ -10,5 +9,5 @@ def factorial(n):
         return n*factorial(n-1)
 
 
-n = int(input("Enter a number\t"))
-print("The factorial of ",n,"is", factorial(n))
+n = int(input("Enter a number:\t"))
+print("The factorial of ",n,"is: ", factorial(n))
