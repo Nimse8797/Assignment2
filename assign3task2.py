da99f22112459c544892d8a54be0d7900de8e627
Vaishnavi Nimse <vaishnavinimse8797@gmail.com>
2025-05-27 18:57:59 +0530

    Update assign3task2.py

--- assign3task2.py
+++ assign3task2.py
@@ -1,5 +1,5 @@
 import math
-n=int(input("Enter a number\t"))
-print(math.sqrt(n))
-print(math.log(n))
-print(math.sin(n))
+n=int(input("Enter a number:\t"))
+print("Square root: ", math.sqrt(n))
+print("Logarithm: ",math.log(n))
+print("Sine: ",math.sin(n))
